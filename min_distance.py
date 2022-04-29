0b1616605c3cd76db74f0aad8746f9dfe9ec2bf0
Terrence Wong <terrencewong223@gmail.com>
2022-04-29 11:09:21 -0700

    Update min_distance.py

--- min_distance.py
+++ min_distance.py
@@ -1,5 +1,11 @@
+# Time complexity: M * log(N) 
+# Iterate through the shorter list and binary search the longer list to find smallest distance
 def minDistance(list1, list2):
     min_dist = float("inf")
+    
+    shorter_list = list1 if len(list1) <= len(list2) else list2
+    longer_list = list1 if len(list1) > len(list2) else list2
+
     def findMinDistance(num: int, list2: typing.List[str]) -> None:
             nonlocal min_dist
             mid = len(list2) // 2
@@ -10,7 +16,7 @@ def minDistance(list1, list2):
             elif num < list2[mid]:
                 findMinDistance(num, list2[:mid])
 
-    for i in list1:
-        findMinDistance(i, list2)
+    for i in shorter_list:
+        findMinDistance(i, longer_list)
     
     return min_dist
